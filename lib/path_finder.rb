3663ff574e635898dc21c0a06640f6a1febf6836
enzo rivello <vincenzo.rivello@gmail.com>
2013-05-24 19:01:54 +0100

    library update, commenting and refactoring

--- lib/path_finder.rb
+++ lib/path_finder.rb
@@ -1,23 +1,26 @@
 require "path_finder/version"
 require 'yaml'
 
-
 module PathFinder
- 	def self.start!(source, destination)
+ 	def self.start!(source, destination, web_request=false)
  		test = PathFinder::Graph.new
- 		test.load_from_yaml(File.join(File.dirname(File.expand_path(__FILE__)), "../assets/tube_list.yml"))
- 		test.print_shortest_paths(source, destination)
+ 		test.load_from_yaml()
+ 		test.print_shortest_paths(source, destination, web_request)
  	end
+
+ 	# Graph Initialization
  	class Graph
  		attr_reader :graph, :nodes, :prev_station, :distance_modifier
 		def initialize
-			@graph = {}	 # the graph // {node => { edge1 => weight, edge2 => weight}, node2 => ...
+			@graph = {}	
 			@nodes = Array.new		 
 			@INFINITY = 1 << 64 
 			@tube_list = []
 		end
 			
-		def add_edge(source, target, weight, line) 	# s= source, t= target, w= weight
+		# Edge data structure: { "Euston" => { "Warren Street" => [ 1, ["Northern", "Victoria"] ] ]}
+		# In this way i got track of all the lines between 2 stations
+		def add_edge(source, target, weight, line)
 			if (not @graph.has_key?(source))	 
 				@graph[source] = {target => [weight, [line]  ] }		 
 			elsif(@graph[source].has_key?(target))
@@ -47,8 +50,7 @@ module PathFinder
 			end	
 		end
 		
-		# based of wikipedia's pseudocode: http://en.wikipedia.org/wiki/Dijkstra's_algorithm
-		
+		# Base Dijkstra implementation 
 		def dijkstra(source)
 			@distance = {}
 			@prev = {}
@@ -76,12 +78,10 @@ module PathFinder
 				q = q - [u]
 				@graph[u].keys.each do |v| 
 					current_line = @graph[u][v][1]
-					if @distance[u] > 0
+					modifier = 0 # setting station change modifier to a default 0
+					if @distance[u] > 0 
 						modifier = changed_line?(@prev[u][1], current_line) == true ? 1 : 0
-					else
-						modifier = 0
 					end
-
 					alt = @distance[u] + @graph[u][v][0] + modifier
 					
 					if(alt < @distance[v])
@@ -92,8 +92,8 @@ module PathFinder
 			end
 		end
 
+		# Helper method to check if there is a possible line change between station
 		def changed_line?(previous_line, current_line)
-			#puts "#{current_line} include #{previous_line} ? "
 			previous_line.each do |line|
 				if current_line.include?(line)
 					return false
@@ -102,8 +102,7 @@ module PathFinder
 			return true
 		end
 		
-		# To print the full shortest route to a node
-		
+		# print path method
 		def print_path(dest)
 			if @prev[dest] != -1
 				print_path @prev[dest][0]
@@ -112,8 +111,8 @@ module PathFinder
 		end
 	
 
-		def load_from_yaml(source)
-			lines = YAML.load_file(source)
+		def load_from_yaml
+			lines = YAML.load_file(File.join(File.dirname(File.expand_path(__FILE__)), "../assets/tube_list.yml"))
 			lines.each do |line|
 				stations = line[1]
 				stations.each_with_index do |station,index|
@@ -123,7 +122,6 @@ module PathFinder
 		end
 		
 		# Gets all shortests paths using dijkstra
-		
 		def shortest_paths(source, dest)
 			@source = source
 			dijkstra source
@@ -131,20 +129,24 @@ module PathFinder
 			return @distance[dest]
 		end
 
-		def print_shortest_paths(source,dest)
+
+		def print_shortest_paths(source,dest, web_request)
 			check_data_integrity(source, dest)
 			total_distance = shortest_paths(source, dest)
 			stations_changed = (total_distance - @tube_list.size) > 0 ? (total_distance - @tube_list.size) : 0;
 			if total_distance != @INFINITY
-				puts "Source: #{source}, Destination: #{dest}\n"
-				puts "#{@tube_list.join(', ')}"
-				puts "#{total_distance} #{@tube_list.size}"
-				puts "\nDistance: #{total_distance}, you changed station #{stations_changed} times"
+				if !web_request
+					puts "#{@tube_list.join(', ')}"
+					puts "\nDistance: #{total_distance}, you changed station #{stations_changed} times"
+				else
+					return "#{@tube_list.join(', ')}"
+				end
 			else
 				puts "NO PATH"
 			end
 		end
 
+		# Helper method to check if the stations are the correct ones
 		def check_data_integrity(source, dest)
 			station_list = @graph.map {|x| x.first}
 			errors = []
